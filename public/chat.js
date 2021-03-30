86df7be183d4a859bc1247e690d5dcc3eb443ccb
maxi <maxi_rosanda@hotmail.com>
2021-03-30 15:55:19 -0300

    sexto

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,6 @@
 const socket = io(); // Ya podemos empezar a usar los sockets desde el cliente :)
 const fecha = new Date();
- 
+
 const enviarMensaje = () =>{
     let paquete = { mail: document.getElementById("mail").value,
      mensaje: document.getElementById("mensaje").value,
@@ -9,12 +9,13 @@ const enviarMensaje = () =>{
     socket.emit('paquete', paquete)
 } 
 socket.on('vermensajes', data => {
-
+    
     let texto =""
     data.forEach((el,index) =>{
       texto += `<h3>Mail : ${el.mail} mensaje ${el.mensaje} fecha ${el.fecha} </h3><br>`    
    })
    document.getElementById("contenedordatos").innerHTML=texto
+
 })
 
 
--- public/lista.js
+++ public/lista.js
@@ -2,7 +2,6 @@ const socket = io()
 
 
 socket.on('lista', data => {
-
     let texto =""
     data.forEach((el,index) =>{
       texto += `<h3>id : ${el.id} title ${el.title} price ${el.price} price ${el.thumbnail} </h3><br>`    
